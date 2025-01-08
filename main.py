c21dbe6a42622aefcc696ed85589d492f5d2617d
YayBlaze <blazeannison@icloud.com>
2025-01-08 13:45:01 -0800

    feat: fixed await usage and added more detailed logging

--- main.py
+++ main.py
@@ -61,6 +61,7 @@ class ImageSpam:
         self.url = None
         self.delay = None
         self.toggle = False
+        self.startTime = None
     def setUser(self,  user):
         self.user = user
     def setURL(self, url):
@@ -69,19 +70,21 @@ class ImageSpam:
         self.dealy = delay
     def setToggle(self, toggle):
         self.toggle = toggle
+    def setStartTime(self, time):
+        self.startTime = time
 
 ImageVictims = [ImageSpam() for i in range (10)]
 Victims = [Spam() for i in range(10)]
 
 async def spam(interation):
     print('Function sucess \nVictims:', ", ".join([i.user.display_name for i in Victims if i.user != None]))
-    await interation.response.send_message("The spamming has commenced...")
-    await loop(interation)
+    interation.response.send_message("The spamming has commenced...")
+    loop(interation)
     
 async def loop(interation):
     for i in Victims:
         if i.toggle:
-            print('Loop run for ', i.user,'\nStart time: ',i.start,'\nCurrent Time',time.time())
+            print('Loop run for ', i.user,'\nType: Text','\nMessage: ',i.msg,'\nStart time: ',i.start,'\nCurrent Time',time.time())
             if i.waiting_time:
                 duration_secconds = time.time() - i.start
                 msg = 'Minute '+str(round(duration_secconds//60))+' of waiting for '+i.msg
@@ -94,29 +97,29 @@ async def loop(interation):
                 print('meassage: ', msg, '\ncount: ', i.count)
             embed = discord.Embed(title=msg)
             i.increment_count()
-            if i.type == 0: await i.user.send(embed=embed)
-            elif i.type == 1: await interation.channel.send(content=f"<@{i.user.id}>", embed=embed)
+            if i.type == 0: i.user.send(embed=embed)
+            elif i.type == 1: interation.channel.send(content=f"<@{i.user.id}>", embed=embed)
             
     time.sleep(Victims[0].delay)
-    await loop(interation)
+    loop(interation)
 
 async def image_spam(interation):
     print('Function sucess \nImage Victims:', ", ".join([i.user.display_name for i in ImageVictims if i.user != None]))
-    await interation.response.send_message("The spamming has commenced...")
-    await image_loop(interation)
+    interation.response.send_message("The spamming has commenced...")
+    image_loop(interation)
 
 async def image_loop(interation):
     for i in ImageVictims:
         if i.toggle:
-            print("Spamming: ",i.user.display_name)
+            print('Loop run for ', i.user,'\nType: Image','\nURL: ',i.url,'\nStart time: ',i.start,'\nCurrent Time',time.time())
             async with aiohttp.ClientSession() as session:
                 async with session.get(i.url) as resp:
                     if resp.status != 200:
-                        return await interation.channel.send('Could not download file...')
+                        return interation.channel.send('Could not download file...')
                     data = io.BytesIO(await resp.read())
-                    await interation.channel.send(content=f"<@{i.user.id}>",file=discord.File(data, 'cool_image.png'))
+                    interation.channel.send(content=f"<@{i.user.id}>",file=discord.File(data, 'cool_image.png'))
     time.sleep(Victims[0].delay)
-    await image_loop(interation)
+    image_loop(interation)
             
 #run bot
 @client.event
@@ -131,10 +134,12 @@ async def on_message(message: Message) -> None:
     user_message: str = message.content
     if username == 'yayblaze' and user_message == '!stopspam':
         print('-------------------------------\nATTEMPTING STOP\n-------------------------------')
-        await message.delete()
+        message.delete()
         for i in Victims:
             i.toggle = False
-        await message.channel.send(content="Stopped All Spamming")
+        for i in ImageVictims:
+            i.toggle = False
+        message.channel.send(content="Stopped All Spamming")
         print('-------------------------------\nSTOPED\n-------------------------------')
 
 @tree.command(
@@ -151,21 +156,21 @@ async def spam_cmnd(interation, user:discord.Member, message: str, delay: int, w
                 i.set_delay(delay)
                 i.set_toggle(True)
                 i.set_start(time.time())
-                if wait and count: await interation.response.send_message(content="You cannot turn on wait and count", ephermial=True)
+                if wait and count: interation.response.send_message(content="You cannot turn on wait and count", ephermial=True)
                 i.set_waiting_time_toggle(wait)
                 i.set_count_toggle(count)
-                if type < 0 or type > 1: await interation.response.send_message(content="Invalid Type!", ephemeral=True)
+                if type < 0 or type > 1: interation.response.send_message(content="Invalid Type!", ephemeral=True)
                 i.set_type(type)
                 break
         await spam(interation)
-    else: await interation.response.send_message(content="You don't have the perms to do that (L)", ephemeral=True)
+    else: interation.response.send_message(content="You don't have the perms to do that (L)", ephemeral=True)
 
 @tree.command(
     name="spam-image",
     description="Like spam command but with an image url | usable by yayblaze only",
 )
 async def image_cmd(interation, user:discord.Member, url: str, delay: int):
-    if interation.user.id != 749431660168216650: return await interation.response.send_message(content="You don't have the perms to do that (L)", ephemeral=True)
+    if interation.user.id != 749431660168216650: return interation.response.send_message(content="You don't have the perms to do that (L)", ephemeral=True)
     print(time.time(),": Image Spam Command Run")
     for i in ImageVictims:
         if not i.toggle:
@@ -173,8 +178,9 @@ async def image_cmd(interation, user:discord.Member, url: str, delay: int):
             i.setUser(user)
             i.setURL(url)
             i.setDelay(delay)
+            i.setStartTime(time.time())
             break
-    await image_spam(interation)
+    image_spam(interation)
 
 @tree.command(
         name="info",
@@ -185,7 +191,7 @@ async def info(interation):
     embed = discord.Embed(title="YaySpam", description="This is a discord bot I made for spamming my friends", colour=discord.Colour.yellow())
     embed.add_field(name="Command",value="The /spam command, used to start the bot, can only be run by YayBlaze. If you really want to use it ask me. I might make these docs usefull at some point but i'm the only one who can use it now so i'm not going to", inline=False)
     embed.add_field(name="Use",value="If you want me to spam someone lmk I can spam anyone who you want as long as their in a discord I have admin perms in.", inline=False)
-    await interation.response.send_message(embed=embed)
+    interation.response.send_message(embed=embed)
     
     
 @tree.command(
@@ -197,12 +203,12 @@ async def stop_spam(interation, index: int, type: int):
     if interation.user.id == 749431660168216650:
         if type == 0: i = Victims[index]
         elif type == 1: i = ImageVictims[index]
-        else: return await interation.response.send_message(content="That's not a valid type.", ephemeral=True)
+        else: return interation.response.send_message(content="That's not a valid type.", ephemeral=True)
         if i.toggle:
             i.toggle_spam()
-            await interation.response.send_message(content=f"Stopped Spamming {i.user.display_name}.", ephmeral=True)
-        else: await interation.response.send_message(content='This is not a valid index', ephemeral=True)
-    else: await interation.response.send_message(content="You don't have the perms to do that (L)", ephemeral=True)
+            interation.response.send_message(content=f"Stopped Spamming {i.user.display_name}.", ephmeral=True)
+        else: interation.response.send_message(content='This is not a valid index', ephemeral=True)
+    else: interation.response.send_message(content="You don't have the perms to do that (L)", ephemeral=True)
 
  
 #main entry point
